a36557090d0bdd1b5ed0d2744d209781e544fa23
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-07 22:47:47 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import Base
-from models.base_model import BaseModel
-from sqlalchemy import Column
-from sqlalchemy import String
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Represents a user for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table user.
-    """
+    """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    email = Column('email', String(128), nullable=False)
+    password = Column('password', String(128), nullable=False)
+    first_name = Column('first_name', String(128), nullable=True, default="NULL")
+    last_name = Column('last_name', String(128), nullable=True, default="NULL")
+    # backref may need to be back_populates?Below line commented out bc console
+    # would not run with it in. This line was implemented in Task 8
+    places = relationship("Place", cascade="delete", backref="user")
+    # Below line is commented out for caution and was added in Task 9
+    reviews = relationship("Review", cascade="delete", backref="user")
