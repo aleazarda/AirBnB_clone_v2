f0ce094522bd07517015583e8f863b4f480f0d66
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-11 01:12:40 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Table
+""" Amenity Module for HBNB project """
+import os
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
-
+from models.base_model import BaseModel, Base
 class Amenity(BaseModel, Base):
-    """ class Amenity inherits from BaseModel and Base """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
-    # name = ""
+    """Represents an amenity data set."""
+    __tablename__ = 'amenities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
