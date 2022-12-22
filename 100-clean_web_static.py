294bc68f98bbd86766e860309bf0a18d690d61c9
aleazarda <44554709+aleazarda@users.noreply.github.com>
2022-12-22 22:25:40 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,35 +1,63 @@
-#!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
+#!/usr/bin/python3
+""" test file """
+import os.path
+import os
+from fabric.api import *
+from fabric.operations import run, put, sudo
+import time
+env.hosts = ['35.185.103.0', '35.237.21.105']
 
-apt-get update
-apt-get -y install nginx
-# Install Nginx if it not already installed
-mkdir -p /data/web_static/releases/test/
-# Create the folder /data/ if it doesn’t already exist
-# Create the folder /data/web_static/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
-mkdir -p /data/web_static/shared/
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
-echo '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' > /data/web_static/releases/test/index.html
-# Create a fake HTML file /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Create a symbolic link /data/web_static/current linked to the...
-# .../data/web_static/releases/test/ folder. If the symbolic link already...
-# ...exists, it should be deleted and recreated every time the script is ran.
-chown -hR ubuntu:ubuntu /data/
-# Give ownership of the /data/ folder to the ubuntu user AND group (you can
-# assume this user and group exist). This should be recursive; everything
-# inside should be created/owned by this user/group.
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-# Update the Nginx configuration to serve the content of
-# /data/web_static/current/ to hbnb_static
-# (ex: https://mydomainname.tech/hbnb_static).
-# Don’t forget to restart Nginx after updating the configuration:
-service nginx restart
+
+def do_pack():
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(timestr))
+        return ("versions/web_static_{}.tgz".format(timestr))
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    if (os.path.isfile(archive_path) is False):
+        return False
+
+    try:
+        nconfig = archive_path.split("/")[-1]
+        ndir = ("/data/web_static/releases/" + nconfig.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(ndir))
+        run("sudo tar -xzf /tmp/{} -C {}".format(nconfig, ndir))
+        run("sudo rm /tmp/{}".format(nconfig))
+        run("sudo mv {}/web_static/* {}/".format(ndir, ndir))
+        run("sudo rm -rf {}/web_static".format(ndir))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(ndir))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    try:
+        archive_address = do_pack()
+        val = do_deploy(archive_address)
+        return val
+    except:
+        return False
+
+
+def do_clean(number=0):
+    number = 1 if int(number) == 0 else int(number)
+
+    files = sorted(os.listdir("versions"))
+    [files.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(j) for j in files)]
+
+    with cd("data/web_static/releases"):
+        files = run("ls -tr").split()
+        files = [j for j in files if "web_static_" in j]
+        [files.pop() for i in range(number)]
+        [run("sudo rm -rf ./{}".format(j)) for j in files]
